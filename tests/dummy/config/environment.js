7f428f60a3c867aabe7e08fc91b610902e5681d3
Gmfholley <gmfholley@gmail.com>
2017-05-15 11:54:11 -0500

    Upgrade/handle deprecation warnings

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -29,7 +29,8 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.testPicURL = '/tests/dummy/public';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
